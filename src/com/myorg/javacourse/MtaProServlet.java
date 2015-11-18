943a49b5d1155454f8fb7947c3e5a16b95e3154d
lironlevi <levyliron123@gmail.com>
2015-11-18 11:24:58 +0200

    Ex3

--- src/com/myorg/javacourse/MtaProServlet.java
+++ src/com/myorg/javacourse/MtaProServlet.java
@@ -1,14 +1,16 @@
 package com.myorg.javacourse;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MtaProServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello ");
+		resp.setContentType("text/html");
 		
-
+		MathCalc mCalc = new MathCalc(50, 30, 50, 20, 13);
+		
+		resp.getWriter().println(mCalc.getResult());
 	}
 }
 
