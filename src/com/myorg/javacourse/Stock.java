a23af9290dfea26a6e52d4f9da3974d5e22acfce
lironlevi <levyliron123@gmail.com>
2015-11-24 13:37:04 +0200

    ex04

--- src/com/myorg/javacourse/Stock.java
+++ src/com/myorg/javacourse/Stock.java
@@ -1,17 +1,16 @@
 package com.myorg.javacourse;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
-@SuppressWarnings("deprecation")
 
 public class Stock {
 	
-   private int day;
-   private int month;
-   private int year; 
 
 	private String  symbol;
 	private float ask;
 	private float bid;
+	
+	SimpleDateFormat dateFormat=new SimpleDateFormat ("MM/dd/yyyy");
 	
 	public String getSymbol() {
 		return symbol;
@@ -35,17 +34,15 @@ public class Stock {
 	public Date getdate() {
 		return date;
 	}
-	public void setdate() {
-		this.day = date.getDate();
-		this.month=date.getMonth();
-		this.year=date.getYear();
+	public void setdate(Date date) {
+		this.date=date;
 }
 	private Date date= new Date();
 	
 	  public String getHtmlDescription()
 	    {
 	    	String result1 = "<b>Stock symbol</b>: " + getSymbol() + " <b>Bid</b>: " + getBid() + " <b> ask </b>: " + getAsk()  
-	    			+ " <b>Date</b>: "+ day + "/" + (month+1) + "/" + (year+1900); 
+	    			+ " <b>Date</b>: "+ dateFormat.format(getdate())+ "<br>";
 			return result1;
 	    }
 	    
--- src/com/myorg/javacourse/StockDetailsServlet.java
+++ src/com/myorg/javacourse/StockDetailsServlet.java
@@ -1,11 +1,13 @@
 package com.myorg.javacourse;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class StockDetailsServlet extends HttpServlet {
+	@SuppressWarnings("deprecation")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
 		
@@ -13,20 +15,21 @@ public class StockDetailsServlet extends HttpServlet {
 		stock1.setSymbol("PIH");
 		stock1.setAsk((float) 13.1);
 		stock1.setBid((float) 12.4);
-		stock1.setdate();
+		stock1.setdate(new Date("11/15/2014"));
 				
 		Stock stock2 = new Stock();
 		stock2.setSymbol("AAL");
 		stock2.setAsk((float) 5.78);
 		stock2.setBid((float) 5.5);
-		stock2.setdate();
+		stock2.setdate(new Date("11/15/2014"));
+
 
 
 		Stock stock3 = new Stock();
 		stock3.setSymbol("CAAS");
 		stock3.setAsk((float) 32.2 );
 		stock3.setBid((float) 31.5);
-		stock3.setdate();
+		stock3.setdate(new Date("11/15/2014"));
 
 			
 		resp.getWriter().println(stock1.getHtmlDescription() + "<br> " + 
